826270266c7167a5a71cd87160ff4ecc8ec1cdb6
GautamDash1 <dashgautam331@gmail.com>
2024-03-17 21:51:47 +0530

    login

--- e_sikshya/src/App.js
+++ e_sikshya/src/App.js
@@ -1,12 +1,17 @@
+import React ,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Login } from "./login";
 import { Register } from "./register";
 
 function App() {
+  const [currentForm , setcurrentForm]=useState('login');
   return (
     <div className="App">
-      <Login />
+      {
+        currentForm == "login" ? <Login /> : <Register/>
+      }
+      
     </div>
   );
 }
--- e_sikshya/src/login.jsx
+++ e_sikshya/src/login.jsx
@@ -9,12 +9,15 @@ export const Login = () => {
         console.log(email);
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <label for="email">email</label>
-            <input value={"email"} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Enter the email." id="email" name="email"/>
-            <label for="password">password</label>
-            <input value={"pass"} onChange={(e) => setPass(e.target.value)}type="password" placeholder="Enterpassword" id="password" name="password"/>
-            <button type = "submit">LogIn</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit}>
+                <label for="email">email</label>
+                <input value={"email"} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Enter the email." id="email" name="email"/>
+                <label for="password">password</label>
+                <input value={"pass"} onChange={(e) => setPass(e.target.value)}type="password" placeholder="Enterpassword" id="password" name="password"/>
+                <button type = "submit">LogIn</button>
+            </form>
+            <button>Don't have an account? Register here</button>
+        </>
     )
 }
