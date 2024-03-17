a6f527619033c42530018005531aa89a40b70230
GautamDash1 <dashgautam331@gmail.com>
2024-03-17 20:21:36 +0530

    done

---
+++ e_sikshya/src/login.jsx
@@ -0,0 +1,20 @@
+import React ,{useState} from "react";
+
+export const Login = () => {
+    const [email,setEmail]=useState('');
+    const [pass,setPass]=useState('');
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        console.log(email);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label for="email">email</label>
+            <input value={"email"} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Enter the email." id="email" name="email"/>
+            <label for="password">password</label>
+            <input value={"pass"} onChange={(e) => setPass(e.target.value)}type="password" placeholder="Enterpassword" id="password" name="password"/>
+            <button type = "submit">LogIn</button>
+        </form>
+    )
+}
