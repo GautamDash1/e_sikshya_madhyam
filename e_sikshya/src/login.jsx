08e27f5d7fa80aad50909daec0d365830e29ea0b
GautamDash1 <dashgautam331@gmail.com>
2024-03-18 20:06:01 +0530

    login_c

--- e_sikshya/src/login.jsx
+++ e_sikshya/src/login.jsx
@@ -13,9 +13,9 @@ export const Login = (props) => {
             <h2>Login</h2>
             <form className="login-form" onSubmit={handleSubmit}>
                 <label htmlFor="email">E-mail</label>
-                <input value={ email } onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Enter the email." id="email" name="email"/>
+                <input value={ email } onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Enter the Email." id="email" name="email"/>
                 <label htmlFor="password">Password</label>
-                <input value={ pass } onChange={(e) => setPass(e.target.value)}type="password" placeholder="Enter password" id="password" name="password"/>
+                <input value={ pass } onChange={(e) => setPass(e.target.value)}type="password" placeholder="Enter Password" id="password" name="password"/>
                 <button type = "submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here</button>
